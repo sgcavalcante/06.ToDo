a64e7ccd786e692d27557f06dddfd18858d092ec
eu <sgcdani@gmail.com>
2024-05-31 16:04:43 -0300

    add grafico

--- apps/to_do/urls.py
+++ apps/to_do/urls.py
@@ -1,7 +1,10 @@
 # core/urls.py
 from django.urls import path
-from apps.to_do.views import home
+from apps.to_do.views import home,monitortemp,line_chart_view,line_chart_page
 
 urlpatterns = [
     path('', home, name='home'),
+    path('monitortemp', monitortemp, name='monitortemp'),
+    path('line-chart/', line_chart_view, name='line_chart'),
+    path('line-chart-page', line_chart_page, name='line_chart_page'),
 ]
--- setup/urls.py
+++ setup/urls.py
@@ -21,4 +21,5 @@ from apps.to_do import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apps.to_do.urls')),
+    
 ]
