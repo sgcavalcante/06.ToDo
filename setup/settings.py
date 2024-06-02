af3b68ff5f6904faa1474f1d594f08fb4632d501
eu <sgcdani@gmail.com>
2024-06-02 11:30:10 -0300

    add tabela sensor

--- setup/settings.py
+++ setup/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = ['*','https://web-production-f8f8d.up.railway.app/']
+ALLOWED_HOSTS = ['*','https://web-production-f8f8d.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-f8f8d.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-f8f8d.up.railway.app']
 
 
 # Application definition
