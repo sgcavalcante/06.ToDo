6e82ef3a5dd990f1f2d359da2ed2f00bea0f4954
eu <sgcdani@gmail.com>
2024-06-08 09:07:45 -0300

    inclusao de botao para deletar dados do banco, alteracao de timezone, e melhorias

--- setup/settings.py
+++ setup/settings.py
@@ -138,10 +138,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
