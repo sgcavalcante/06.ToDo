01a3f65f9162b4432b3dd46ffaf79ac2d7731fc5
eu <sgcdani@gmail.com>
2024-06-15 16:39:36 -0300

    peso

--- apps/to_do/urls.py
+++ apps/to_do/urls.py
@@ -1,6 +1,6 @@
 # core/urls.py
 from django.urls import path,include
-from apps.to_do.views import home,monitortemp,sensor_data,receive_data,plot_view,deletar_dados,peso_view ,deletar_dados_peso
+from apps.to_do.views import home,monitortemp,sensor_data,receive_data,plot_view,deletar_dados,peso_view,deletar_dados_peso
 urlpatterns = [ 
     path('', home, name='home'),
     path('monitortemp', monitortemp, name='monitortemp'),
@@ -8,6 +8,6 @@ urlpatterns = [
     path('api/receive-data/', receive_data, name='receive_data'),
     path('plot/', plot_view, name='plot_view'),
     path('deletar_dados/', deletar_dados, name='deletar_dados'),
-    path('peso/', peso_view, name='pesdo_view'),
+    path('peso/', peso_view, name='peso_view'),
     path('deletar_dado_peso/', deletar_dados_peso, name='deletar_dados_peso'),
 ]
