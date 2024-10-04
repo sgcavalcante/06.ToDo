9a155d18a69d7563c34b234136d661a931e735e0
eu <sgcdani@gmail.com>
2024-10-04 00:09:37 -0300

    atualizacao

--- setup/settings.py
+++ setup/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = ['web-production-f8f8d.up.railway.app','sanicousintario.up.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['ventosolares.com.br','web-production-f8f8d.up.railway.app','sanicousintario.up.railway.app','127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-f8f8d.up.railway.app','https://sanicousintario.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-f8f8d.up.railway.app','https://sanicousintario.up.railway.app','https://www.ventosolares.com.br']
 
 
 # Application definition
