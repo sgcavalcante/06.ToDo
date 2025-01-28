5ccbd8026312d20088aafc3bc0e6be04c8a3848e
eu <sgcdani@gmail.com>
2025-01-28 08:16:14 -0300

    atualiuzação das variaveis de banco

--- setup/settings.py
+++ setup/settings.py
@@ -103,6 +103,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=20)}
+PGHOST=os.getenv("PGHOST")          # monorail.proxy.rlwy.net
+PGUSER=os.getenv("PGUSER")          # postgres
+PGPASSWORD=os.getenv("PGPASSWORD")  # ErflndgNadXEJEwnVEeaLXvpdpZfmEQW
+PGDATABASE=os.getenv("PGDATABASE")  # railway
+PGPORT=os.getenv("PORT")            # 29466
 '''
 DATABASES = {
     'default': {
