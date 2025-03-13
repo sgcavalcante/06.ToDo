f8778c72ce201d4053e73ad971d44476f23eef31
eu <sgcdani@gmail.com>
2025-03-13 09:25:50 -0300

    teste

--- setup/settings.py
+++ setup/settings.py
@@ -101,14 +101,14 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=20)}
+#DATABASE_URL = os.getenv("DATABASE_URL")
+#DATABASES = {'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=20)}
 
-'''
+
 PGHOST="postgres.railway.internal"
 PGPORT="5432"
 PGUSER="postgres"
-PGPASSWORD=""
+PGPASSWORD="MXaDDFUtVkbFbCkYcvKiwwkPuEOzBfjI"
 PGDATABASE="railway"
 
 #ssads
@@ -124,7 +124,7 @@ DATABASES = {
         'PORT': os.getenv('PGPORT','5432'),
     }
 }
-'''
+
 
 
 
